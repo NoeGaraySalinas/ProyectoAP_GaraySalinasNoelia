6453b10f9eb75ee0ea9c0f93865b37efbaee5349
NoeGaraySalinas <109987989+NoeGaraySalinas@users.noreply.github.com>
2023-03-26 18:17:53 -0300

    variables de base de datos

--- src/main/java/com/portfolio/ngs/Security/Service/RolService.java
+++ src/main/java/com/portfolio/ngs/Security/Service/RolService.java
@@ -10,13 +10,13 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.portfolio.ngs.Security.Repository.iRolRepository;
+import com.portfolio.ngs.Security.Repository.IRolRepository;
 
 @Service
 @Transactional
 public class RolService {
     @Autowired
-    iRolRepository irolRepository;
+    IRolRepository irolRepository;
     
     public Optional<Rol> getByRolNombre(RolNombre rolNombre){
         return irolRepository.findByRolNombre(rolNombre);
--- src/main/java/com/portfolio/ngs/Security/Service/UsuarioService.java
+++ src/main/java/com/portfolio/ngs/Security/Service/UsuarioService.java
@@ -9,13 +9,13 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.portfolio.ngs.Security.Repository.iUsuarioRepository;
+import com.portfolio.ngs.Security.Repository.IUsuarioRepository;
 
 @Service
 @Transactional
 public class UsuarioService {
     @Autowired
-    iUsuarioRepository iusuarioRepository;
+    IUsuarioRepository iusuarioRepository;
     
     public Optional<Usuario> getByNombreUsuario(String nombreUsuario){
         return iusuarioRepository.findByNombreUsuario(nombreUsuario);
